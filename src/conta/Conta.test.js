c30768eaded93902a89fe3bccd6335c2711cffd3
gmartpad <gabrielmpwork@gmail.com>
2021-04-12 23:42:28 -0300

    Simulando uma função

--- src/conta/Conta.test.js
+++ src/conta/Conta.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 
 import Conta from './Conta';
 
@@ -11,5 +11,14 @@ describe('Componente de Conta', () => {
 
         expect(saldo.textContent).toBe('R$ 1000');
 
+    })
+    it('Chama a função de realizar transação, quando o botão é clicado', () => {
+        const funcaoRealizarTransação = jest.fn()
+        
+        render(<Conta saldo={1000} realizarTransacao={funcaoRealizarTransação} />)
+
+        fireEvent.click(screen.getByText('Realizar operação'));
+
+        expect(funcaoRealizarTransação).toHaveBeenCalled();
     })
 })
